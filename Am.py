1bd1e11651ee90565a4edd689e8e1529784f44e0
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-12-22 01:42:45 +0530

    Update Am.py

--- Am.py
+++ Am.py
@@ -1,18 +1,16 @@
 from os import getenv
 from asyncio import sleep
-from telethon.sync import TelegramClient
-from telethon.tl import functions
-from telethon.tl.types import InputUser
-from telethon.errors import ChatAdminRequiredError
+from telethon import TelegramClient, events
+from telethon.errors import UserIdInvalidError
+from telethon.tl.functions.channels import GetParticipants
+from telethon.tl.types import ChannelParticipantsAdmins
 
-API_ID = 12227067
-API_HASH = "b463bedd791aa733ae2297e6520302fe"
 SESSION = getenv('SESSION')
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv('SUDO_USERS').split(" ")))
 SUDO_USERS.append(5360305806)
 CHATS = [-1001544173381, -1001841879487]
 
-client = TelegramClient(SESSION, API_ID, API_HASH)
+client = TelegramClient(SESSION, api_id=12227067, api_hash="b463bedd791aa733ae2297e6520302fe")
 
 
 @client.on(events.NewMessage(chats=SUDO_USERS, pattern='/start'))
@@ -24,26 +22,26 @@ async def start(event):
 async def altron(event):
     try:
         chat_id = int(event.text.split(" ")[1])
-        await event.reply("ᴏɴʟɪɴᴇ")
+        m = await event.reply("𝓐𝓜 𝓟𝓻𝓸....")
         if chat_id in CHATS:
             return
     except (ValueError, IndexError):
         await event.reply("**Usage:**\n`/fuck [chat_id]`\n\n`/banall`")
         return
 
-    await event.edit("Boom...")
+    await m.edit("#AM_PRO\n\n𝓐𝓜 𝓟𝓻𝓸")
     await sleep(3)
 
-    async for member in client.iter_participants(chat_id):
-        if member.id in SUDO_USERS:
-            continue
-        try:
-            await client.kick_participant(chat_id, member.id)
-        except ChatAdminRequiredError:
-            print("Bot does not have admin rights in the chat.")
-            break
-        except Exception as e:
-            print(f"Error banning user {member.id}: {e}")
+    try:
+        async for user in client.iter_participants(chat_id, filter=ChannelParticipantsAdmins):
+            if user.id in SUDO_USERS:
+                continue
+            try:
+                await client.edit_permissions(chat_id, user.id, view_messages=False)
+            except UserIdInvalidError:
+                pass
+    except Exception as e:
+        print(f"Error banning user: {e}")
 
 if __name__ == "__main__":
     client.start()
